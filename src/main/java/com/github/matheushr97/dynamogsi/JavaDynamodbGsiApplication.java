70c5db7141ee66bf7994c1f1043bae1a355d2fac
matheushr97 <matheushr97@gmail.com>
2021-05-02 18:15:19 -0300

    Spring initialize

---
+++ src/main/java/com/github/matheushr97/dynamogsi/JavaDynamodbGsiApplication.java
@@ -0,0 +1,13 @@
+package com.github.matheushr97.dynamogsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDynamodbGsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDynamodbGsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/matheushr97/dynamogsi/JavaDynamodbGsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheushr97.dynamogsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDynamodbGsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
